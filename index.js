200589699a3bc81a2ae51995a6848fa65867ffd0
Hans Meiser <info.lrn@pagehunter.de>
2019-05-25 17:35:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!";
+}
+
+function happyHolidaysTo(name) {
+  return `Happy holidays, ${name}`;
+}
+
+function happyHoliday(holiday, name) {
+  return `Happ ${holiday}, ${name}!`;
+}
+
+function holdayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`;
+}s
